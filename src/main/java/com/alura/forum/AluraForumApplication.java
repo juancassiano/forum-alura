874d24e473203c2396088e81ed46c62b03c900f3
juancassiano <juancassiano@hotmail.com>
2023-06-05 13:58:00 -0300

    application port

--- src/main/java/com/alura/forum/AluraForumApplication.java
+++ src/main/java/com/alura/forum/AluraForumApplication.java
@@ -1,24 +1,13 @@
 package com.alura.forum;
 
-import static java.lang.String.valueOf;
-
-import java.util.Collections;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AluraForumApplication {
 
-	@Value("${PORT:8080}")
-	private static int port;
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(AluraForumApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", valueOf(port)));
-		app.run(args);
-
-
+		SpringApplication.run(AluraForumApplication.class, args);
 	}
 
 }
